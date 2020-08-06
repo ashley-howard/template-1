3cf5d179dfc0233015e9342d5f9720dd9558c661
Ashley Howard <me@ashley.how>
2020-08-06 17:57:43 +0100

    add menu btn and functionality

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+if (document.getElementById('typed')) {
+    var typed = new Typed('#typed', {
+        // stringsElement: '#typed-strings',
+        strings: ['This could be your website', 'This could be your project', 'This could be your brand', 'This could be your company'],
+        // showCursor: false,
+        typeSpeed: 50
+    });
+}
+
+const menuBtn = document.getElementById('menu-btn')
+const links = document.getElementById('links')
+
+menuBtn.onclick = function () { openMenu() }
+
+function openMenu() {
+    if (links.style.display == 'flex') {
+        links.style.display = 'none';
+        menuBtn.src = 'media/menu.svg'
+
+    } else {
+        links.style.display = 'flex';
+        menuBtn.src = 'media/close.svg'
+    }
+}
--- scripts/typed.js
+++
@@ -1,8 +0,0 @@
-if (document.getElementById('typed')) {
-    var typed = new Typed('#typed', {
-        // stringsElement: '#typed-strings',
-        strings: ['This could be your website', 'This could be your project', 'This could be your brand', 'This could be your company'],
-        // showCursor: false,
-        typeSpeed: 50
-    });
-}
