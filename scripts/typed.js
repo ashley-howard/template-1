2e575bbfa7bc17c4cd77377cf4b6e8a104efa7a9
Ashley Howard <me@ashley.how>
2020-08-06 15:33:31 +0100

    first commit

---
+++ scripts/typed.js
@@ -0,0 +1,8 @@
+if (document.getElementById('typed')) {
+    var typed = new Typed('#typed', {
+        // stringsElement: '#typed-strings',
+        strings: ['This could be your website', 'This could be your project', 'This could be your brand', 'This could be your company'],
+        // showCursor: false,
+        typeSpeed: 50
+    });
+}
